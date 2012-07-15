ca58cda053d36f8d9303c64e7e14319244069791
Holger Arnold <git@harnold.org>
2012-07-15 20:21:48 +0200

    Prefix template-like macro parameters with '_'
    This helps avoiding name collisions and lets these parameters stand out
    a bit more.

--- gc/alg.h
+++ gc/alg.h
@@ -1,140 +1,140 @@
 #ifndef GC_ALG_H
 #define GC_ALG_H
 
-#define gc_for_each_pos(C, pos, range) \
-    for ((pos) = C##_range_begin(range); \
-         (pos) != C##_range_end(range); \
-         C##_forward(&(pos)))
+#define gc_for_each_pos(_C, pos, range) \
+    for ((pos) = _C##_range_begin(range); \
+         (pos) != _C##_range_end(range); \
+         _C##_forward(&(pos)))
 
-#define gc_for_each_pos_rev(C, pos, tmp, range) \
-    for ((tmp) = C##_range_end(range); \
-         (tmp) != C##_range_begin(range) ? (pos) = C##_prev(tmp), 1 : 0; \
-         C##_backward(&(tmp)))
+#define gc_for_each_pos_rev(_C, pos, tmp, range) \
+    for ((tmp) = _C##_range_end(range); \
+         (tmp) != _C##_range_begin(range) ? (pos) = _C##_prev(tmp), 1 : 0; \
+         _C##_backward(&(tmp)))
 
-#define gc_for_each_pos_indexed(C, i, pos, range) \
-    for ((pos) = C##_range_begin(range), (i) = 0; \
-         (pos) != C##_range_end(range); \
-         C##_forward(&(pos)), (i)++)
+#define gc_for_each_pos_indexed(_C, i, pos, range) \
+    for ((pos) = _C##_range_begin(range), (i) = 0; \
+         (pos) != _C##_range_end(range); \
+         _C##_forward(&(pos)), (i)++)
 
-#define gc_for_each_pos_safe(C, pos, tmp, range) \
-    for ((pos) = C##_range_begin(range), (tmp) = C##_next(pos); \
-         (pos) != C##_range_end(range); \
-         (pos) = (tmp), C##_forward(&(tmp)))
+#define gc_for_each_pos_safe(_C, pos, tmp, range) \
+    for ((pos) = _C##_range_begin(range), (tmp) = _C##_next(pos); \
+         (pos) != _C##_range_end(range); \
+         (pos) = (tmp), _C##_forward(&(tmp)))
 
-#define gc_swap(C, pos1, pos2) \
+#define gc_swap(_C, pos1, pos2) \
     do { \
-        C##_elem_t tmp = C##_get(pos1); \
-        C##_set(pos1, C##_get(pos2)); \
-        C##_set(pos2, tmp); \
+        _C##_elem_t tmp = _C##_get(pos1); \
+        _C##_set(pos1, _C##_get(pos2)); \
+        _C##_set(pos2, tmp); \
     } while (0)
 
-#define gc_for_each(C, range, f) \
+#define gc_for_each(_C, range, f) \
     do { \
-        C##_pos_t pos; \
-        gc_for_each_pos(C, pos, range) \
-            (f)(C##_get(pos)); \
+        _C##_pos_t pos; \
+        gc_for_each_pos(_C, pos, range) \
+            (f)(_C##_get(pos)); \
     } while (0)
 
-#define gc_find(C, range, val, pos) \
+#define gc_find(_C, range, val, pos) \
     do { \
-        gc_for_each_pos(C, *(pos), range) { \
-            if (C##_get(*(pos)) == (val)) \
+        gc_for_each_pos(_C, *(pos), range) { \
+            if (_C##_get(*(pos)) == (val)) \
                 break; \
         } \
     } while (0)
 
-#define gc_find_eq(C, range, eq, val, pos) \
+#define gc_find_eq(_C, range, eq, val, pos) \
     do { \
-        gc_for_each_pos(C, *(pos), range) { \
-            if ((eq)(C##_get(*(pos)), (val))) \
+        gc_for_each_pos(_C, *(pos), range) { \
+            if ((eq)(_C##_get(*(pos)), (val))) \
                 break; \
         } \
     } while (0)
 
-#define gc_find_if(C, range, pred, pos) \
+#define gc_find_if(_C, range, pred, pos) \
     do { \
-        gc_for_each_pos(C, *(pos), range) { \
-            if ((pred)(C##_get(*(pos)))) \
+        gc_for_each_pos(_C, *(pos), range) { \
+            if ((pred)(_C##_get(*(pos)))) \
                 break; \
         } \
     } while (0)
 
-#define gc_count(C, range, val, n) \
+#define gc_count(_C, range, val, n) \
     do { \
-        C##_pos_t pos; \
+        _C##_pos_t pos; \
         *(n) = 0; \
-        gc_for_each_pos(C, *(pos), range) { \
-            if (C##_get(*(pos)) == (val)) \
+        gc_for_each_pos(_C, *(pos), range) { \
+            if (_C##_get(*(pos)) == (val)) \
                 (*(n))++; \
         } \
     } while (0)
 
-#define gc_count_eq(C, range, eq, val, n) \
+#define gc_count_eq(_C, range, eq, val, n) \
     do { \
-        C##_pos_t pos; \
+        _C##_pos_t pos; \
         *(n) = 0; \
-        gc_for_each_pos(C, *(pos), range) { \
-            if ((eq)(C##_get(*(pos)), (val))) \
+        gc_for_each_pos(_C, *(pos), range) { \
+            if ((eq)(_C##_get(*(pos)), (val))) \
                 (*(n))++; \
         } \
     } while (0)
 
-#define gc_count_if(C, range, pred, n) \
+#define gc_count_if(_C, range, pred, n) \
     do { \
-        C##_pos_t pos; \
+        _C##_pos_t pos; \
         *(n) = 0; \
-        gc_for_each_pos(C, *(pos), range) { \
-            if ((pred)(C##_get(*(pos)))) \
+        gc_for_each_pos(_C, *(pos), range) { \
+            if ((pred)(_C##_get(*(pos)))) \
                 (*(n))++; \
         } \
     } while (0)
 
-#define gc_copy_front(C1, range, C2, cont, copy_elem) \
+#define gc_copy_front(_C1, range, _C2, cont, copy_elem) \
     do { \
-        C##_pos_t pos; \
-        gc_for_each_pos(C1, pos, range) { \
-            C2##_insert_front(cont, (copy_elem)(C1##_get(pos))); \
+        _C1##_pos_t pos; \
+        gc_for_each_pos(_C1, pos, range) { \
+            _C2##_insert_front(cont, (copy_elem)(_C1##_get(pos))); \
         } \
     } while (0)
 
-#define gc_copy_front(C1, range, C2, cont, copy_elem) \
+#define gc_copy_front(_C1, range, _C2, cont, copy_elem) \
     do { \
-        C##_pos_t pos; \
-        gc_for_each_pos(C1, pos, range) { \
-            C2##_insert_front(cont, (copy_elem)(C1##_get(pos))); \
+        _C1##_pos_t pos; \
+        gc_for_each_pos(_C1, pos, range) { \
+            _C2##_insert_front(cont, (copy_elem)(_C1##_get(pos))); \
         } \
     } while (0)
 
-#define gc_copy_before(C1, range, C2, cont, pos, copy_elem) \
+#define gc_copy_before(_C1, range, _C2, cont, pos, copy_elem) \
     do { \
-        C##_pos_t pos; \
-        gc_for_each_pos(C1, pos, range) { \
-            C2##_insert_before(cont, pos, (copy_elem)(C1##_get(pos))); \
+        _C1##_pos_t pos; \
+        gc_for_each_pos(_C1, pos, range) { \
+            _C2##_insert_before(cont, pos, (copy_elem)(_C1##_get(pos))); \
         } \
     } while (0)
 
-#define gc_copy_after(C1, range, C2, cont, pos, copy_elem) \
+#define gc_copy_after(_C1, range, _C2, cont, pos, copy_elem) \
     do { \
-        C##_pos_t pos; \
-        gc_for_each_pos(C1, pos, range) { \
-            C2##_insert_after(cont, pos, (copy_elem)(C1##_get(pos))); \
+        _C1##_pos_t pos; \
+        gc_for_each_pos(_C1, pos, range) { \
+            _C2##_insert_after(cont, pos, (copy_elem)(_C1##_get(pos))); \
         } \
     } while (0)
 
-#define gc_fill(C, range, val) \
+#define gc_fill(_C, range, val) \
     do { \
-        C##_pos_t pos; \
-        gc_for_each_pos(C, pos, range) { \
-            C##_set(pos, val); \
+        _C##_pos_t pos; \
+        gc_for_each_pos(_C, pos, range) { \
+            _C##_set(pos, val); \
         } \
     } while(0)
 
-#define gc_generate(C, range, generate_elem) \
+#define gc_generate(_C, range, generate_elem) \
     do { \
         int i; \
-        C##_pos_t pos; \
-        gc_for_each_pos_indexed(C, i, pos, range) { \
-            C##_set(pos, (generate_elem)(i)) \
+        _C##_pos_t pos; \
+        gc_for_each_pos_indexed(_C, i, pos, range) { \
+            _C##_set(pos, (generate_elem)(i)) \
         } \
     } while (0)
 
--- gc/list.h
+++ gc/list.h
@@ -29,98 +29,98 @@
          (node) != gc_list_end(list); \
          (node) = (tmp), (tmp) = (node)->next)
 
-#define GC_GENERATE_LIST_TYPES(C, T) \
+#define GC_GENERATE_LIST_TYPES(_C, _T) \
 \
-typedef struct C C##_t; \
-typedef struct C##_node *C##_pos_t; \
-typedef struct C##_range C##_range_t; \
-typedef T C##_elem_t; \
+typedef struct _C _C##_t; \
+typedef struct _C##_node *_C##_pos_t; \
+typedef struct _C##_range _C##_range_t; \
+typedef _T _C##_elem_t; \
 \
-struct C##_node { \
-    struct C##_node *next; \
-    struct C##_node *prev; \
-    T elem; \
+struct _C##_node { \
+    struct _C##_node *next; \
+    struct _C##_node *prev; \
+    _T elem; \
 }; \
 \
-struct C##_range { \
-    struct C##_node *begin; \
-    struct C##_node *end; \
+struct _C##_range { \
+    struct _C##_node *begin; \
+    struct _C##_node *end; \
 }; \
 \
-struct C { \
-    struct C##_node end; \
-    void (*destroy_elem)(T); \
+struct _C { \
+    struct _C##_node end; \
+    void (*destroy_elem)(_T); \
 };
 
-#define GC_GENERATE_LIST_FUNCTIONS_STATIC(C, T) \
-    GC_GENERATE_LIST_FUNCTION_DEFS(C, T, static)
+#define GC_GENERATE_LIST_FUNCTIONS_STATIC(_C, _T) \
+    GC_GENERATE_LIST_FUNCTION_DEFS(_C, _T, static)
 
-#define GC_GENERATE_LIST_FUNCTIONS_STATIC_INLINE(C, T) \
-    GC_GENERATE_LIST_FUNCTION_DEFS(C, T, static inline)
+#define GC_GENERATE_LIST_FUNCTIONS_STATIC_INLINE(_C, _T) \
+    GC_GENERATE_LIST_FUNCTION_DEFS(_C, _T, static inline)
 
-#define GC_GENERATE_LIST_FUNCTIONS_EXTERN_H(C, T) \
-    GC_GENERATE_LIST_FUNCTION_DECLS(C, T, )
+#define GC_GENERATE_LIST_FUNCTIONS_EXTERN_H(_C, _T) \
+    GC_GENERATE_LIST_FUNCTION_DECLS(_C, _T, )
 
-#define GC_GENERATE_LIST_FUNCTIONS_EXTERN_C(C, T) \
-    GC_GENERATE_LIST_FUNCTION_DEFS(C, T, )
+#define GC_GENERATE_LIST_FUNCTIONS_EXTERN_C(_C, _T) \
+    GC_GENERATE_LIST_FUNCTION_DEFS(_C, _T, )
 
-#define GC_GENERATE_LIST_FUNCTIONS_EXTERN_INLINE_H(C, T) \
-    GC_GENERATE_LIST_FUNCTION_DEFS(C, T, inline)
+#define GC_GENERATE_LIST_FUNCTIONS_EXTERN_INLINE_H(_C, _T) \
+    GC_GENERATE_LIST_FUNCTION_DEFS(_C, _T, inline)
 
-#define GC_GENERATE_LIST_FUNCTIONS_EXTERN_INLINE_C(C, T) \
-    GC_GENERATE_LIST_FUNCTION_DECLS(C, T, )
+#define GC_GENERATE_LIST_FUNCTIONS_EXTERN_INLINE_C(_C, _T) \
+    GC_GENERATE_LIST_FUNCTION_DECLS(_C, _T, )
 
-#define GC_GENERATE_LIST_FUNCTION_DECLS(C, T, _funspecs) \
+#define GC_GENERATE_LIST_FUNCTION_DECLS(_C, _T, _funspecs) \
 \
-_funspecs void init_##C(struct C *list, void (*destroy_elem)(T));  \
-_funspecs void destroy_##C(struct C *list);  \
-_funspecs void C##_link_nodes(struct C##_node *prev, struct C##_node *next);  \
-_funspecs void C##_unlink_node(struct C##_node* node);  \
-_funspecs C##_pos_t C##_begin(C##_t *list);  \
-_funspecs C##_pos_t C##_end(C##_t *list);  \
-_funspecs C##_range_t C##_all(C##_t *list);  \
-_funspecs C##_range_t C##_range(C##_pos_t begin, C##_pos_t end);  \
-_funspecs C##_pos_t C##_range_begin(C##_range_t range);  \
-_funspecs C##_pos_t C##_range_end(C##_range_t range);  \
-_funspecs C##_range_t C##_range_from_pos(C##_t *list, C##_pos_t pos);  \
-_funspecs C##_range_t C##_range_to_pos(C##_t *list, C##_pos_t pos);  \
-_funspecs bool C##_range_empty(C##_range_t range);  \
-_funspecs bool C##_empty(C##_t *list);  \
-_funspecs T C##_front(C##_t *list);  \
-_funspecs T C##_back(C##_t *list);  \
-_funspecs T C##_get(C##_pos_t pos);  \
-_funspecs void C##_set(C##_pos_t pos, T val);  \
-_funspecs C##_pos_t C##_next(C##_pos_t pos);  \
-_funspecs C##_pos_t C##_prev(C##_pos_t pos);  \
-_funspecs void C##_forward(C##_pos_t *pos);  \
-_funspecs void C##_backward(C##_pos_t *pos);  \
-_funspecs C##_pos_t C##_insert(C##_t *list, C##_pos_t pos, T val);  \
-_funspecs void C##_insert_front(C##_t *list, T val);  \
-_funspecs void C##_insert_back(C##_t *list, T val);  \
-_funspecs C##_pos_t C##_release(C##_t *list, C##_pos_t pos);  \
-_funspecs C##_pos_t C##_remove(C##_t *list, C##_pos_t pos);  \
-_funspecs void C##_remove_front(C##_t *list);  \
-_funspecs void C##_remove_back(C##_t *list);  \
-_funspecs void C##_clear(C##_t *list);  \
-_funspecs void C##_move(C##_t *dest_list, C##_pos_t dest_pos, C##_t *src_list, C##_pos_t src_pos);  \
-_funspecs void C##_move_front(C##_t *dest_list, C##_t *src_list, C##_pos_t pos);  \
-_funspecs void C##_move_back(C##_t *dest_list, C##_t *src_list, C##_pos_t pos);  \
-_funspecs void C##_splice(C##_t *dest_list, C##_pos_t pos, C##_t *src_list, C##_range_t range);  \
-_funspecs void C##_splice_front(C##_t *dest_list, C##_t *src_list, C##_range_t range);  \
-_funspecs void C##_splice_back(C##_t *dest_list, C##_t *src_list, C##_range_t range);
+_funspecs void init_##_C(struct _C *list, void (*destroy_elem)(_T));  \
+_funspecs void destroy_##_C(struct _C *list);  \
+_funspecs void _C##_link_nodes(struct _C##_node *prev, struct _C##_node *next);  \
+_funspecs void _C##_unlink_node(struct _C##_node* node);  \
+_funspecs _C##_pos_t _C##_begin(_C##_t *list);  \
+_funspecs _C##_pos_t _C##_end(_C##_t *list);  \
+_funspecs _C##_range_t _C##_all(_C##_t *list);  \
+_funspecs _C##_range_t _C##_range(_C##_pos_t begin, _C##_pos_t end);  \
+_funspecs _C##_pos_t _C##_range_begin(_C##_range_t range);  \
+_funspecs _C##_pos_t _C##_range_end(_C##_range_t range);  \
+_funspecs _C##_range_t _C##_range_from_pos(_C##_t *list, _C##_pos_t pos);  \
+_funspecs _C##_range_t _C##_range_to_pos(_C##_t *list, _C##_pos_t pos);  \
+_funspecs bool _C##_range_empty(_C##_range_t range);  \
+_funspecs bool _C##_empty(_C##_t *list);  \
+_funspecs _T _C##_front(_C##_t *list);  \
+_funspecs _T _C##_back(_C##_t *list);  \
+_funspecs _T _C##_get(_C##_pos_t pos);  \
+_funspecs void _C##_set(_C##_pos_t pos, _T val);  \
+_funspecs _C##_pos_t _C##_next(_C##_pos_t pos);  \
+_funspecs _C##_pos_t _C##_prev(_C##_pos_t pos);  \
+_funspecs void _C##_forward(_C##_pos_t *pos);  \
+_funspecs void _C##_backward(_C##_pos_t *pos);  \
+_funspecs _C##_pos_t _C##_insert(_C##_t *list, _C##_pos_t pos, _T val);  \
+_funspecs void _C##_insert_front(_C##_t *list, _T val);  \
+_funspecs void _C##_insert_back(_C##_t *list, _T val);  \
+_funspecs _C##_pos_t _C##_release(_C##_t *list, _C##_pos_t pos);  \
+_funspecs _C##_pos_t _C##_remove(_C##_t *list, _C##_pos_t pos);  \
+_funspecs void _C##_remove_front(_C##_t *list);  \
+_funspecs void _C##_remove_back(_C##_t *list);  \
+_funspecs void _C##_clear(_C##_t *list);  \
+_funspecs void _C##_move(_C##_t *dest_list, _C##_pos_t dest_pos, _C##_t *src_list, _C##_pos_t src_pos);  \
+_funspecs void _C##_move_front(_C##_t *dest_list, _C##_t *src_list, _C##_pos_t pos);  \
+_funspecs void _C##_move_back(_C##_t *dest_list, _C##_t *src_list, _C##_pos_t pos);  \
+_funspecs void _C##_splice(_C##_t *dest_list, _C##_pos_t pos, _C##_t *src_list, _C##_range_t range);  \
+_funspecs void _C##_splice_front(_C##_t *dest_list, _C##_t *src_list, _C##_range_t range);  \
+_funspecs void _C##_splice_back(_C##_t *dest_list, _C##_t *src_list, _C##_range_t range);
 
-#define GC_GENERATE_LIST_FUNCTION_DEFS(C, T, _funspecs) \
+#define GC_GENERATE_LIST_FUNCTION_DEFS(_C, _T, _funspecs) \
 \
-_funspecs void init_##C(struct C *list, void (*destroy_elem)(T)) \
+_funspecs void init_##_C(struct _C *list, void (*destroy_elem)(_T)) \
 { \
     list->end.next = &list->end; \
     list->end.prev = &list->end; \
     list->destroy_elem = destroy_elem; \
 } \
 \
-_funspecs void destroy_##C(struct C *list) \
+_funspecs void destroy_##_C(struct _C *list) \
 { \
-    struct C##_node *node, *tmp; \
+    struct _C##_node *node, *tmp; \
 \
     if (list->destroy_elem) { \
         gc_list_for_each_node(node, list) \
@@ -131,112 +131,112 @@ _funspecs void destroy_##C(struct C *list) \
         free(node); \
 } \
 \
-_funspecs void C##_link_nodes(struct C##_node *prev, struct C##_node *next) \
+_funspecs void _C##_link_nodes(struct _C##_node *prev, struct _C##_node *next) \
 { \
     prev->next = next; \
     next->prev = prev; \
 } \
 \
-_funspecs void C##_unlink_node(struct C##_node* node) \
+_funspecs void _C##_unlink_node(struct _C##_node* node) \
 { \
-    C##_link_nodes(node->prev, node->next); \
+    _C##_link_nodes(node->prev, node->next); \
 } \
 \
-_funspecs C##_pos_t C##_begin(C##_t *list) \
+_funspecs _C##_pos_t _C##_begin(_C##_t *list) \
 { \
     return gc_list_begin(list); \
 } \
 \
-_funspecs C##_pos_t C##_end(C##_t *list) \
+_funspecs _C##_pos_t _C##_end(_C##_t *list) \
 { \
     return gc_list_end(list); \
 } \
 \
-_funspecs C##_range_t C##_all(C##_t *list) \
+_funspecs _C##_range_t _C##_all(_C##_t *list) \
 { \
-    return (struct C##_range) gc_list_all(list); \
+    return (struct _C##_range) gc_list_all(list); \
 } \
 \
-_funspecs C##_range_t C##_range(C##_pos_t begin, C##_pos_t end) \
+_funspecs _C##_range_t _C##_range(_C##_pos_t begin, _C##_pos_t end) \
 { \
-    return (struct C##_range) gc_list_range(begin, end); \
+    return (struct _C##_range) gc_list_range(begin, end); \
 } \
 \
-_funspecs C##_pos_t C##_range_begin(C##_range_t range) \
+_funspecs _C##_pos_t _C##_range_begin(_C##_range_t range) \
 { \
     return gc_list_range_begin(range); \
 } \
 \
-_funspecs C##_pos_t C##_range_end(C##_range_t range) \
+_funspecs _C##_pos_t _C##_range_end(_C##_range_t range) \
 { \
     return gc_list_range_end(range); \
 } \
 \
-_funspecs C##_range_t C##_range_from_pos(C##_t *list, C##_pos_t pos) \
+_funspecs _C##_range_t _C##_range_from_pos(_C##_t *list, _C##_pos_t pos) \
 { \
-    return (struct C##_range) gc_list_range_from_pos(list, pos); \
+    return (struct _C##_range) gc_list_range_from_pos(list, pos); \
 } \
 \
-_funspecs C##_range_t C##_range_to_pos(C##_t *list, C##_pos_t pos) \
+_funspecs _C##_range_t _C##_range_to_pos(_C##_t *list, _C##_pos_t pos) \
 { \
-    return (struct C##_range) gc_list_range_to_pos(list, pos); \
+    return (struct _C##_range) gc_list_range_to_pos(list, pos); \
 } \
 \
-_funspecs bool C##_range_empty(C##_range_t range) \
+_funspecs bool _C##_range_empty(_C##_range_t range) \
 { \
     return gc_list_range_empty(range); \
 } \
 \
-_funspecs bool C##_empty(C##_t *list) \
+_funspecs bool _C##_empty(_C##_t *list) \
 { \
     return gc_list_begin(list) == gc_list_end(list); \
 } \
 \
-_funspecs T C##_front(C##_t *list) \
+_funspecs _T _C##_front(_C##_t *list) \
 { \
-    assert(!C##_empty(list)); \
+    assert(!_C##_empty(list)); \
     return gc_list_begin(list)->elem; \
 } \
 \
-_funspecs T C##_back(C##_t *list) \
+_funspecs _T _C##_back(_C##_t *list) \
 { \
-    assert(!C##_empty(list)); \
+    assert(!_C##_empty(list)); \
     return gc_list_end(list)->prev->elem; \
 } \
 \
-_funspecs T C##_get(C##_pos_t pos) \
+_funspecs _T _C##_get(_C##_pos_t pos) \
 { \
     return pos->elem; \
 } \
 \
-_funspecs void C##_set(C##_pos_t pos, T val) \
+_funspecs void _C##_set(_C##_pos_t pos, _T val) \
 { \
     pos->elem = val; \
 } \
 \
-_funspecs C##_pos_t C##_next(C##_pos_t pos) \
+_funspecs _C##_pos_t _C##_next(_C##_pos_t pos) \
 { \
     return pos->next; \
 } \
 \
-_funspecs C##_pos_t C##_prev(C##_pos_t pos) \
+_funspecs _C##_pos_t _C##_prev(_C##_pos_t pos) \
 { \
     return pos->prev; \
 } \
 \
-_funspecs void C##_forward(C##_pos_t *pos) \
+_funspecs void _C##_forward(_C##_pos_t *pos) \
 { \
     *pos = (*pos)->next; \
 } \
 \
-_funspecs void C##_backward(C##_pos_t *pos) \
+_funspecs void _C##_backward(_C##_pos_t *pos) \
 { \
     *pos = (*pos)->prev; \
 } \
 \
-_funspecs C##_pos_t C##_insert(C##_t *list, C##_pos_t pos, T val) \
+_funspecs _C##_pos_t _C##_insert(_C##_t *list, _C##_pos_t pos, _T val) \
 { \
-    struct C##_node *node; \
+    struct _C##_node *node; \
 \
     if (!(node = malloc(sizeof(*node)))) { \
         GC_ERROR(errno, "Allocating memory for list node failed"); \
@@ -244,58 +244,58 @@ _funspecs C##_pos_t C##_insert(C##_t *list, C##_pos_t pos, T val) \
     } \
 \
     node->elem = val; \
-    C##_link_nodes(pos->prev, node); \
-    C##_link_nodes(node, pos); \
+    _C##_link_nodes(pos->prev, node); \
+    _C##_link_nodes(node, pos); \
     return node; \
 } \
 \
-_funspecs void C##_insert_front(C##_t *list, T val) \
+_funspecs void _C##_insert_front(_C##_t *list, _T val) \
 { \
-    C##_insert(list, gc_list_begin(list), val); \
+    _C##_insert(list, gc_list_begin(list), val); \
 } \
 \
-_funspecs void C##_insert_back(C##_t *list, T val) \
+_funspecs void _C##_insert_back(_C##_t *list, _T val) \
 { \
-    C##_insert(list, gc_list_end(list), val); \
+    _C##_insert(list, gc_list_end(list), val); \
 } \
 \
-_funspecs C##_pos_t C##_release(C##_t *list, C##_pos_t pos) \
+_funspecs _C##_pos_t _C##_release(_C##_t *list, _C##_pos_t pos) \
 { \
-    C##_pos_t next; \
+    _C##_pos_t next; \
 \
     assert(pos != gc_list_end(list)); \
 \
     next = pos->next;
-    C##_unlink_node(pos); \
+    _C##_unlink_node(pos); \
     free(pos); \
     return next; \
 } \
 \
-_funspecs C##_pos_t C##_remove(C##_t *list, C##_pos_t pos) \
+_funspecs _C##_pos_t _C##_remove(_C##_t *list, _C##_pos_t pos) \
 { \
     assert(pos != gc_list_end(list)); \
 \
     if (list->destroy_elem) \
         list->destroy_elem(pos->elem); \
 \
-    return C##_release(list, pos); \
+    return _C##_release(list, pos); \
 } \
 \
-_funspecs void C##_remove_front(C##_t *list) \
+_funspecs void _C##_remove_front(_C##_t *list) \
 { \
-    assert(!C##_empty(list)); \
-    C##_remove(list, gc_list_begin(list)); \
+    assert(!_C##_empty(list)); \
+    _C##_remove(list, gc_list_begin(list)); \
 } \
 \
-_funspecs void C##_remove_back(C##_t *list) \
+_funspecs void _C##_remove_back(_C##_t *list) \
 { \
-    assert(!C##_empty(list)); \
-    C##_remove(list, gc_list_end(list)->prev); \
+    assert(!_C##_empty(list)); \
+    _C##_remove(list, gc_list_end(list)->prev); \
 } \
 \
-_funspecs void C##_clear(C##_t *list) \
+_funspecs void _C##_clear(_C##_t *list) \
 { \
-    struct C##_node *node, *tmp; \
+    struct _C##_node *node, *tmp; \
 \
     if (list->destroy_elem) { \
         gc_list_for_each_node(node, list) \
@@ -305,45 +305,45 @@ _funspecs void C##_clear(C##_t *list) \
     gc_list_for_each_node_safe(node, tmp, list) \
         free(node); \
 \
-    init_##C(list, list->destroy_elem); \
+    init_##_C(list, list->destroy_elem); \
 } \
 \
-_funspecs void C##_move(C##_t *dest_list, C##_pos_t dest_pos, C##_t *src_list, C##_pos_t src_pos) \
+_funspecs void _C##_move(_C##_t *dest_list, _C##_pos_t dest_pos, _C##_t *src_list, _C##_pos_t src_pos) \
 { \
     assert(src_pos != gc_list_end(src_list)); \
 \
-    C##_unlink_node(src_pos); \
-    C##_link_nodes(dest_pos->prev, src_pos); \
-    C##_link_nodes(src_pos, dest_pos); \
+    _C##_unlink_node(src_pos); \
+    _C##_link_nodes(dest_pos->prev, src_pos); \
+    _C##_link_nodes(src_pos, dest_pos); \
 } \
 \
-_funspecs void C##_move_front(C##_t *dest_list, C##_t *src_list, C##_pos_t pos) \
+_funspecs void _C##_move_front(_C##_t *dest_list, _C##_t *src_list, _C##_pos_t pos) \
 { \
     assert(pos != gc_list_end(src_list)); \
-    C##_move(dest_list, gc_list_begin(dest_list), src_list, pos); \
+    _C##_move(dest_list, gc_list_begin(dest_list), src_list, pos); \
 } \
 \
-_funspecs void C##_move_back(C##_t *dest_list, C##_t *src_list, C##_pos_t pos) \
+_funspecs void _C##_move_back(_C##_t *dest_list, _C##_t *src_list, _C##_pos_t pos) \
 { \
     assert(pos != gc_list_end(src_list)); \
-    C##_move(dest_list, gc_list_end(dest_list), src_list, pos); \
+    _C##_move(dest_list, gc_list_end(dest_list), src_list, pos); \
 } \
 \
-_funspecs void C##_splice(C##_t *dest_list, C##_pos_t pos, C##_t *src_list, C##_range_t range) \
+_funspecs void _C##_splice(_C##_t *dest_list, _C##_pos_t pos, _C##_t *src_list, _C##_range_t range) \
 { \
-    C##_link_nodes(range.end->prev, pos); \
-    C##_link_nodes(range.begin->prev, range.end); \
-    C##_link_nodes(pos->prev, range.begin); \
+    _C##_link_nodes(range.end->prev, pos); \
+    _C##_link_nodes(range.begin->prev, range.end); \
+    _C##_link_nodes(pos->prev, range.begin); \
 } \
 \
-_funspecs void C##_splice_front(C##_t *dest_list, C##_t *src_list, C##_range_t range) \
+_funspecs void _C##_splice_front(_C##_t *dest_list, _C##_t *src_list, _C##_range_t range) \
 { \
-    C##_splice(dest_list, gc_list_begin(dest_list), src_list, range); \
+    _C##_splice(dest_list, gc_list_begin(dest_list), src_list, range); \
 } \
 \
-_funspecs void C##_splice_back(C##_t *dest_list, C##_t *src_list, C##_range_t range) \
+_funspecs void _C##_splice_back(_C##_t *dest_list, _C##_t *src_list, _C##_range_t range) \
 { \
-    C##_splice(dest_list, gc_list_end(dest_list), src_list, range); \
+    _C##_splice(dest_list, gc_list_end(dest_list), src_list, range); \
 }
 
 #endif
