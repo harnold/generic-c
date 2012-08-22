7364468e67d0167c837b3ee3e2c086b43fa02a22
Holger Arnold <git@harnold.org>
2012-08-22 14:51:26 +0200

    Add gcl/ringbuf.h

---
+++ gcl/ringbuf.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2012 Holger Arnold.
+ *
+ * Licensed under a modified BSD license.
+ * See the accompanying LICENSE file for details.
+ */
+
+#ifndef GCL_RINGBUF_H
+#define GCL_RINGBUF_H
+
+
+
+#endif
