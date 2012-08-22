bab2e1e5843441ef48bc95dfa4c5bedcf8ae4c58
Holger Arnold <git@harnold.org>
2012-08-22 14:56:29 +0200

    ringbuf: Add data structures and typedefs

--- gcl/ringbuf.h
+++ gcl/ringbuf.h
@@ -8,6 +8,31 @@
 #ifndef GCL_RINGBUF_H
 #define GCL_RINGBUF_H
 
+/* This is a dummy type used only during development. */
+typedef struct { double x, y; } _T;
+
+typedef struct _C _C_t;
+typedef struct _C_pos _C_pos_t;
+typedef struct _C_range _C_range_t;
+
+struct _C_pos {
+    struct _C *buf;
+    _T *ptr;
+};
+
+struct _C_range {
+    struct _C *buf;
+    _T *begin;
+    _T *end;
+};
+
+struct _C {
+    _T *data;
+    _T *data_end;
+    _T *begin;
+    _T *end;
+    void (*destroy_elem)(_T);
+};
 
 
 #endif
