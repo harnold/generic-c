e2ae2287d7062200197670074df2c45eb73e284f
Holger Arnold <git@harnold.org>
2012-08-23 16:23:40 +0200

    ringbuf: Add size-related functions

--- gcl/ringbuf.h
+++ gcl/ringbuf.h
@@ -14,7 +14,10 @@
 
 #include <assert.h>
 #include <errno.h>
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define GCL_RINGBUF_MINIMAL_CAPACITY    (15)
 #define GCL_RINGBUF_INITIAL_CAPACITY    (15)
@@ -57,6 +60,76 @@ struct _C {
 /* This is a dummy macro used only during development. */
 #define _funcspecs
 
+_funcspecs bool __C_contiguous(struct _C *buf);
+_funcspecs void __C_move_data(_T *begin, _T *end, _T *dest);
+_funcspecs _T *__C_do_resize_grow(struct _C *buf, size_t n);
+_funcspecs _T *init__C(struct _C *buf, size_t n, void (*destroy_elem)(_T));
+_funcspecs void destroy__C(struct _C *buf);
+_funcspecs size_t _C_length(_C_t *buf);
+_funcspecs bool _C_empty(_C_t *buf);
+_funcspecs size_t _C_capacity(_C_t *buf);
+_funcspecs size_t _C_max_capacity(void);
+_funcspecs _T *_C_reserve(_C_t *buf, size_t n);
+
+_funcspecs bool __C_contiguous(struct _C *buf)
+{
+    return buf->end - buf->begin >= 0;
+}
+
+_funcspecs void __C_move_data(_T *begin, _T *end, _T *dest)
+{
+    assert(begin <= end);
+
+    size_t n = (size_t) (end - begin);
+
+    if (n > 0)
+        memmove(dest, begin, n * sizeof(_T));
+}
+
+_funcspecs _T *__C_do_resize_grow(struct _C *buf, size_t n)
+{
+    assert(n >= _C_capacity(buf) && n <= _C_max_capacity());
+
+    if (n < GCL_RINGBUF_MINIMAL_CAPACITY)
+        n = GCL_RINGBUF_MINIMAL_CAPACITY;
+
+    if (n == _C_capacity(buf))
+        return buf->data;
+
+    size_t old_cap = _C_capacity(buf);
+    size_t begin_offset = (size_t) (buf->begin - buf->data);
+    size_t end_offset = (size_t) (buf->end - buf->data);
+
+    _T *data = realloc(buf->data, (n + 1) * sizeof(_T));
+
+    if (!data) {
+        GCL_ERROR(errno, "Reallocating memory for ring buffer failed");
+        return NULL;
+    }
+
+    if (!__C_contiguous(buf)) {
+
+        size_t left_part = end_offset;
+        size_t right_part = old_cap - begin_offset;
+
+        if (left_part < right_part && left_part <= n - old_cap) {
+            __C_move_data(data, data + end_offset, data + old_cap);
+            end_offset += old_cap;
+        } else {
+            __C_move_data(data + begin_offset, data + old_cap,
+                          data + n - right_part);
+            begin_offset += n - old_cap;
+        }
+    }
+
+    buf->data = data;
+    buf->data_end = data + n;
+    buf->begin = data + begin_offset;
+    buf->end = data + end_offset;
+
+    return data;
+}
+
 _funcspecs _T *init__C(struct _C *buf, size_t n, void (*destroy_elem)(_T))
 {
     if (n < GCL_RINGBUF_INITIAL_CAPACITY)
@@ -90,6 +163,38 @@ _funcspecs void destroy__C(struct _C *buf)
     }
 
     free(buf->data);
+}
+
+_funcspecs size_t _C_length(_C_t *buf)
+{
+    return __C_contiguous(buf) ?
+        buf->end - buf->begin :
+        buf->end - buf->begin + (buf->data_end - buf->data);
+}
+
+_funcspecs bool _C_empty(_C_t *buf)
+{
+    return buf->begin == buf->end;
+}
+
+_funcspecs size_t _C_capacity(_C_t *buf)
+{
+    return buf->data_end - buf->data - 1;
+}
+
+_funcspecs size_t _C_max_capacity(void)
+{
+    return SIZE_MAX / sizeof(_T) - 1;
+}
+
+_funcspecs _T *_C_reserve(_C_t *buf, size_t n)
+{
+    assert(n <= _C_max_capacity());
+
+    if (n > _C_capacity(buf))
+        return __C_do_resize_grow(buf, n);
+    else
+        return buf->data;
 }
 
 #endif
