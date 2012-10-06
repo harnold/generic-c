e3934fe874601c8a9495f5f7c61c0558094ad287
Holger Arnold <git@harnold.org>
2012-10-06 22:48:34 +0200

    Add error functions
    All gcl functions will use the gcl_warning() and gcl_error() macros to
    output warning and error messages.  By default, the functions called by
    these macros print a diagnostic message to stderr; gcl_error() then
    aborts the program.  The user can chance this behavior by providing
    alternative definitions of these macros before including any gcl header.

---
+++ gcl/error.c
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2012 Holger Arnold.
+ *
+ * Licensed under a modified BSD license.
+ * See the accompanying LICENSE file for details.
+ */
+
+#include <gcl/error.h>
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define PROGNAME_BUFSIZE 64
+
+static char program_name[PROGNAME_BUFSIZE] = "";
+
+/* This function is not thread-safe! */
+void gcl_set_program_name(const char *name)
+{
+    strncpy(program_name, name, PROGNAME_BUFSIZE - 1);
+    program_name[PROGNAME_BUFSIZE - 1] = '\0';
+}
+
+void _gcl_warning(const char *file, unsigned int line, const char *func,
+                  const char *format, ...)
+{
+    va_list args;
+
+    fflush(stdout);
+
+    if (strlen(program_name) > 0)
+        fprintf(stderr, "%s (%s:%u, %s): ", program_name, file, line, func);
+
+    va_start(args, format);
+    vfprintf(stderr, format, args);
+    va_end(args);
+
+    fputc('\n', stderr);
+}
+
+void _gcl_error(const char *file, unsigned int line, const char *func,
+                int errnum, const char *format, ...)
+{
+    va_list args;
+
+    fflush(stdout);
+
+    if (strlen(program_name) > 0)
+        fprintf(stderr, "%s (%s:%u, %s): ", program_name, file, line, func);
+
+    va_start(args, format);
+    vfprintf(stderr, format, args);
+    va_end(args);
+
+    if (errnum != 0)
+        fprintf(stderr, ": %s", strerror(errnum));
+
+    fputc('\n', stderr);
+
+    abort();
+}
---
+++ gcl/error.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2012 Holger Arnold.
+ *
+ * Licensed under a modified BSD license.
+ * See the accompanying LICENSE file for details.
+ */
+
+#ifndef GCL_ERROR_H
+#define GCL_ERROR_H
+
+#include <gcl/compat.h>
+
+void gcl_set_program_name(const char *name);
+
+#ifndef gcl_warning
+#define gcl_warning(...) \
+    _gcl_warning(__FILE__, __LINE__, __func__, __VA_ARGS__)
+#endif
+
+#ifndef gcl_error
+#define gcl_error(errnum, ...) \
+    _gcl_error(__FILE__, __LINE__, __func__, errnum, __VA_ARGS__)
+#endif
+
+void _gcl_warning(const char *file, unsigned int line, const char *func,
+                  const char *format, ...)
+    GCL_FORMAT_STRING(4);
+
+void _gcl_error(const char *file, unsigned int line, const char *func,
+                int errnum, const char *format, ...)
+    GCL_FORMAT_STRING(5)
+    GCL_NO_RETURN;
+
+#endif
